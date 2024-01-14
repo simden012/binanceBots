a97c75bb45f28d925618228e6e9d24e1a9c2fc48
Simon Denis <simon.denis@polymtl.ca>
2024-01-14 13:08:24 -0500

    fix : restructuration 1.1

--- python_script/bot_lower_bb.py
+++ python_script/bot_lower_bb.py
@@ -1,10 +1,10 @@
-from bolling_bands_requests import lower_bolling_band, calculate_bolling_bands
-from binance_class import user123
+from python_script.indicators_calculator.bolling_bands_requests import lower_bolling_band, calculate_bolling_bands
+from python_script.Binance_Class.binance_class import user123
 import time
 
-tickers = ['BTCBUSD','OCEANBUSD', 'GALABUSD', 'AGIXBUSD', 'APTBUSD', 'SOLBUSD', 
-'AVAXBUSD', 'ZILBUSD', 'PHBBUSD', 'MANABUSD', 'SANDBUSD', 'SRMBUSD', 'ANCBUSD', 'FTTBUSD',
-'TORNBUSD', 'MIRBUSD', 'FXSBUSD', 'SLPBUSD']
+tickers = ['BTCBUSD', 'OCEANBUSD', 'GALABUSD', 'AGIXBUSD', 'APTBUSD', 'SOLBUSD',
+           'AVAXBUSD', 'ZILBUSD', 'PHBBUSD', 'MANABUSD', 'SANDBUSD', 'SRMBUSD', 'ANCBUSD', 'FTTBUSD',
+           'TORNBUSD', 'MIRBUSD', 'FXSBUSD', 'SLPBUSD']
 interval = '15m'
 
 while True:
@@ -12,15 +12,15 @@ while True:
     for ticker in tickers:
         lower, middle, upper = calculate_bolling_bands(ticker, interval)
         price = user123.get_price(ticker)
-        
+
         difference_lower_mid = middle - lower
 
         difference_price_lower = abs(price - lower)
-        if(difference_price_lower/difference_lower_mid <= 0.5):
+        if (difference_price_lower/difference_lower_mid <= 0.5):
             print(f"{ticker} is at {price} and lower is {round(lower,3)}")
         else:
-            print(f"NOTHING to do for {ticker} with price at {round(price, 3)}$")
+            print(
+                f"NOTHING to do for {ticker} with price at {round(price, 3)}$")
 
     print("-----SLEEPING FOR 30 SECONDS-----")
     time.sleep(30)
-    
--- python_script/trading_bot.py
+++ python_script/trading_bot.py
@@ -1,32 +1,35 @@
-from binance_class import user123
-from bolling_bands_requests import calculate_bolling_bands
-from rsi_binance_requests import calculate_rsi
+from python_script.Binance_Class.binance_class import user123
+from python_script.indicators_calculator.bolling_bands_requests import calculate_bolling_bands
+from python_script.indicators_calculator.rsi_binance_requests import calculate_rsi
 from bitcoin_analysis import analyze_btc_price
 import time
 
-symbols = ['BTCBUSD','ETHBUSD', 'GALABUSD', 'AGIXBUSD', 'APTBUSD', 'SOLBUSD', 
-'AVAXBUSD', 'ZILBUSD', 'PHBBUSD', 'MANABUSD', 'SANDBUSD', 'SRMBUSD', 'ANCBUSD', 'FTTBUSD',
-'TORNBUSD', 'MIRBUSD', 'FXSBUSD', 'SLPBUSD']
+symbols = ['BTCBUSD', 'ETHBUSD', 'GALABUSD', 'AGIXBUSD', 'APTBUSD', 'SOLBUSD',
+           'AVAXBUSD', 'ZILBUSD', 'PHBBUSD', 'MANABUSD', 'SANDBUSD', 'SRMBUSD', 'ANCBUSD', 'FTTBUSD',
+           'TORNBUSD', 'MIRBUSD', 'FXSBUSD', 'SLPBUSD']
 
 interval = '15m'
 
 money_per_trade = 100
-count =0
+count = 0
 while True:
     btc_recent_pourcentage = analyze_btc_price()
     if btc_recent_pourcentage > -1.5:
         for symbol in symbols:
 
-            klines = user123.client.get_klines(symbol = symbols[0], interval=interval)
+            klines = user123.client.get_klines(
+                symbol=symbols[0], interval=interval)
 
             close_price = [float(kline[4]) for kline in klines]
 
-            lower_band, middle_band, upper_band = calculate_bolling_bands(symbol, interval)
+            lower_band, middle_band, upper_band = calculate_bolling_bands(
+                symbol, interval)
             rsi_value = calculate_rsi(symbol, interval, 15)
 
             current_price = user123.get_price(symbol)
             if current_price < lower_band and rsi_value < 30:
-                quantity_buy = user123.cash_to_quantity(symbol, money_per_trade)
+                quantity_buy = user123.cash_to_quantity(
+                    symbol, money_per_trade)
                 # user123.order_market_buy(symbol, quantity_buy)
                 print(f"BOUGHT {quantity_buy} {symbol} at {current_price}$")
 
@@ -36,8 +39,7 @@ while True:
                 # user123.order_market_sell(symbol, quantity_sell)
                 print(f"SOLD {quantity_sell} {symbol} at {current_price}$")
             else:
-                print(f"NOTHING to do for {symbol} with price at {round(current_price, 3)}$ and rsi at {round(rsi_value, 3)}")
+                print(
+                    f"NOTHING to do for {symbol} with price at {round(current_price, 3)}$ and rsi at {round(rsi_value, 3)}")
     print("-----SLEEPING FOR 30 SECONDS-----")
     time.sleep(30)
-
-       
